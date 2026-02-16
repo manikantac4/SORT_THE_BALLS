f1d6f3ca46a0c97789827f63ab40af07efd2ac63
Panduranga <panduranga1797@gmail.com>
2026-02-16 23:28:29 +0530

    chnages

--- src/Startingpage.jsx
+++ src/Startingpage.jsx
@@ -329,7 +329,7 @@ export default function SortTheBallsBackground() {
     /* navigate after zoom animation */
     setTimeout(() => {
 
-      navigate("/loading")
+      navigate("/game")
 
     }, 1200)
 
